0a9c705c14bc84f76003df881cfcb468f45d6041
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2021-11-28 00:12:19 +0000

    Oops

--- Models/Stats_RoundModel.cs
+++ Models/Stats_RoundModel.cs
@@ -8,7 +8,7 @@ namespace ParadisePublicAPI.Models {
         public int round_id { get; set; }
         public DateTime init_datetime { get; set; }
         public DateTime? start_datetime { get; set; }
-        public DateTime? shutdown_dateime { get; set; }
+        public DateTime? shutdown_datetime { get; set; }
         public DateTime? end_datetime { get; set; }
         public string? commit_hash { get; set; }
         public string? game_mode { get; set; }
@@ -21,7 +21,7 @@ namespace ParadisePublicAPI.Models {
             round_id = round.Id;
             init_datetime = round.InitializeDatetime;
             start_datetime = round.StartDatetime;
-            shutdown_dateime = round.ShutdownDatetime;
+            shutdown_datetime = round.ShutdownDatetime;
             end_datetime = round.EndDatetime;
             commit_hash = round.CommitHash;
             game_mode = round.GameMode;
