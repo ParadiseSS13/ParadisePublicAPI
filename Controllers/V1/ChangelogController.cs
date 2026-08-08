bc6ecc341feedeae12146d1c5c84eda5515241ac
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2026-08-08 15:59:12 +0100

    CL models

---
+++ Controllers/V1/ChangelogController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParadisePublicAPI.Database;
+using ParadisePublicAPI.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace ParadisePublicAPI.Controllers.V1 {
+    /// <summary>
+    /// Controller for querying statistics from game rounds
+    /// </summary>
+    [ApiExplorerSettings(GroupName = "v1")]
+    [SwaggerTag("Query historical changelog entries")]
+    [Route("cl")]
+    public class ChangelogController(ParadiseGamedbContext context) : Controller {
+        private readonly ParadiseGamedbContext _context = context;
+
+        /// <summary>
+        /// Gets a list of valid changelog entries.
+        /// </summary>
+        /// <param name="offset">Offset of CL entries to take from. If not specified, will take the 50 most recent PRs merged. If specified, will take the next 50 PRs older than that offset.</param>
+        /// <returns>A list of up to 50 PRs worth of changelog entries.</returns>
+        /// <response code="200">Changelog list successfully retrieved</response>
+        /// <response code="429">Rate limited by server</response>
+        [HttpGet("entries")]
+        public IActionResult GetEntries(int offset) {
+            // Init up here so lines down dont whine
+            List<int> recent_prs = [];
+
+            // Account for optional offset
+            if (offset > 0) {
+                // We got an offset, account for it
+                recent_prs = [.. _context.Changelogs.Where(x => x.PrNumber < offset).Select(x => x.PrNumber).Distinct().OrderByDescending(x => x).Take(50)];
+            } else {
+                // We dont. Just go as normal.
+                recent_prs = [.. _context.Changelogs.Select(x => x.PrNumber).Distinct().OrderByDescending(x => x).Take(50)];
+            }
+
+            List<Changelog> db_cl_entries = [.. _context.Changelogs.Where(x => recent_prs.Contains(x.PrNumber)).OrderByDescending(x => x.Id)];
+            List<ChangelogModel> out_models = [];
+
+            foreach(Changelog cl in db_cl_entries) {
+                ChangelogModel cm = new();
+                cm.FromModel(cl);
+                out_models.Add(cm);
+            }
+
+            // And send it
+            return Ok(out_models);
+        }
+    }
+}
---
+++ Models/ChangelogModel.cs
@@ -0,0 +1,33 @@
+﻿using ParadisePublicAPI.Database;
+using System.Text.Json.Serialization;
+
+namespace ParadisePublicAPI.Models {
+    public class ChangelogModel {
+        [JsonPropertyName("key")]
+        public int RowId { get; set; } // This is named key just to make react accept it
+
+        [JsonPropertyName("prn")]
+        public int PrNumber { get; set; }
+
+        [JsonPropertyName("dm")]
+        public DateTime DateMerged { get; set; }
+
+        [JsonPropertyName("author")]
+        public string Author { get; set; } = string.Empty;
+
+        [JsonPropertyName("clt")]
+        public string ChangelogType { get; set; } = string.Empty;
+
+        [JsonPropertyName("cle")]
+        public string ChangelogEntry { get; set; } = string.Empty;
+
+        public void FromModel(Changelog changelog) {
+            RowId = changelog.Id;
+            PrNumber = changelog.PrNumber;
+            DateMerged = changelog.DateMerged;
+            Author = changelog.Author;
+            ChangelogType = changelog.ClType;
+            ChangelogEntry = changelog.ClEntry;
+        }
+    }
+}
